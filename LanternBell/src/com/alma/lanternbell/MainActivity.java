36d7d5a7c6a9c260b9fa75961cec75a44b73213c
Maxim Kuzovlev <ky3eh.home@gmail.com>
2014-03-24 17:18:59 +0400

    Service implemented

--- LanternBell/src/com/alma/lanternbell/MainActivity.java
+++ LanternBell/src/com/alma/lanternbell/MainActivity.java
@@ -2,14 +2,36 @@ package com.alma.lanternbell;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.Button;
+import com.alma.lanternbell.listener.AbstractButtonListener;
+import com.alma.lanternbell.listener.OffButtonListener;
+import com.alma.lanternbell.listener.OnButtonListener;
 
 public class MainActivity extends Activity
 {
-    /** Called when the activity is first created. */
-    @Override
-    public void onCreate(Bundle savedInstanceState)
-    {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.main);
-    }
+	private static final String	TAG	= "MainActivity";
+		
+	@Override
+	public void onCreate( Bundle savedInstanceState_ )
+	{
+		Log.e( TAG, "onCreate" );
+		
+		super.onCreate( savedInstanceState_ );
+		setContentView( R.layout.main );
+		
+		m_onButton	= (Button)findViewById( R.id.ButtonON );
+		m_offButton	= (Button)findViewById( R.id.ButtonOFF );
+		
+		AbstractButtonListener	onListener	= new OnButtonListener( this );
+		AbstractButtonListener	offListener	= new OffButtonListener( this );
+		
+		m_onButton.setOnClickListener( onListener );
+		m_offButton.setOnClickListener( offListener );
+		
+	}
+	
+	private Button	m_onButton;
+	private Button	m_offButton;
+	
 }
--- LanternBell/src/com/alma/lanternbell/service/BellService.java
+++ LanternBell/src/com/alma/lanternbell/service/BellService.java
@@ -1,10 +1,12 @@
 package com.alma.lanternbell.service;
 
 import android.app.Service;
+import android.content.Context;
 import android.content.Intent;
 import android.os.IBinder;
 import android.telephony.PhoneStateListener;
 import android.telephony.TelephonyManager;
+import android.util.Log;
 
 /**
  * Mar 24, 2014
@@ -12,9 +14,50 @@ import android.telephony.TelephonyManager;
  */
 public class BellService extends Service
 {
+	private static final String	TAG	= "BellService";
+	
+	@Override
+	public void onCreate()
+	{
+		super.onCreate(); //To change body of generated methods, choose Tools | Templates.
+		
+		Log.e( TAG, "onCreate" );
+		
+	}
+	
+	@Override
+	public int onStartCommand( Intent intent_, int flags_, int startId_ )
+	{
+		Log.e( TAG, "onStartCommand" );
+		
+		Context				ctx					= getApplicationContext();
+		TelephonyManager	telephonyManager	= (TelephonyManager) ctx.getSystemService( Context.TELEPHONY_SERVICE );
+		
+		telephonyManager.listen( m_stateListener, PhoneStateListener.LISTEN_CALL_STATE );
+		
+		return Service.START_STICKY;
+		
+	}
+	
+	@Override
+	public void onDestroy()
+	{
+		Log.e( TAG, "OnDestroy" );
+		
+		Context				ctx					= getApplicationContext();
+		TelephonyManager	telephonyManager	= (TelephonyManager) ctx.getSystemService( Context.TELEPHONY_SERVICE );
+		
+		telephonyManager.listen( m_stateListener, PhoneStateListener.LISTEN_NONE );
+		
+		super.onDestroy(); //To change body of generated methods, choose Tools | Templates.
+		
+	}
+	
 	@Override
 	public IBinder onBind( Intent intent )
 	{
+		Log.e( TAG, "onBind" );
+		
 		return null;	// This service does not support binding
 		
 	}
@@ -24,14 +67,28 @@ public class BellService extends Service
 		@Override
 		public void onCallStateChanged( int state_, String incomingNumber_ )
 		{
+			Log.e( TAG, "onCallStateChanged" );
+			
 			switch( state_ )
 			{
 			case TelephonyManager.CALL_STATE_RINGING:
+				Log.d( TAG, "CALL_STATE_RINGING" );
 				break;
+				
+			case TelephonyManager.CALL_STATE_IDLE:
+				Log.d( TAG, "CALL_STATE_IDLE" );
+				break;
+				
+			case TelephonyManager.CALL_STATE_OFFHOOK:
+				Log.d( TAG, "CALL_STATE_OFFHOOK" );
+				break;
+				
 			}
 			
 		}
 		
 	}
+	
+	private final CallStateListener	m_stateListener	= new CallStateListener();
 
 }
