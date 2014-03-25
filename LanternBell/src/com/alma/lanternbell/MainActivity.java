6174f8a91f47e8a88c60e2df4ddac8531684e273
Maxim Kuzovlev <ky3eh.home@gmail.com>
2014-03-25 14:37:08 +0400

    1.1.0 One button + check is service running at the start.

--- LanternBell/src/com/alma/lanternbell/MainActivity.java
+++ LanternBell/src/com/alma/lanternbell/MainActivity.java
@@ -1,6 +1,8 @@
 package com.alma.lanternbell;
 
 import android.app.Activity;
+import android.app.ActivityManager;
+import android.content.ComponentName;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
@@ -9,10 +11,13 @@ import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
 import com.alma.lanternbell.service.BellService;
+import java.util.List;
 
 public class MainActivity extends Activity
 {
-	private static final String	TAG	= "MainActivity";
+	private static final String	TAG			= "MainActivity";
+	private static final String	CAPTION_ON	= "On";
+	private static final String	CAPTION_OFF	= "Off";
 	
 	@Override
 	public void onCreate( Bundle savedInstanceState_ )
@@ -22,42 +27,80 @@ public class MainActivity extends Activity
 		super.onCreate( savedInstanceState_ );
 		setContentView( R.layout.main );
 		
-		m_onButton	= (Button)findViewById( R.id.ButtonON );
-		m_offButton	= (Button)findViewById( R.id.ButtonOFF );
+		m_isRunning		= isServiceRunning( BellService.class );
+		m_serviceButton	= (Button)findViewById( R.id.ButtonSERVICE );
 		
-		m_onButton.setOnClickListener( new OnClickListener()
-										{
-											public void onClick( View v )
+		String	caption	= true == m_isRunning ? CAPTION_OFF : CAPTION_ON;
+		
+		m_serviceButton.setText( caption );
+		
+		m_serviceButton.setOnClickListener( new OnClickListener()
 											{
-												Log.i( TAG, "onClick" );
+												public void onClick( View v )
+												{
+													Log.i( TAG, "onClick" );
 
-												Context	ctx		= getApplicationContext();
-												Intent	intent	= new Intent( ctx, BellService.class );
+													ProcessOnClick();
 
-												startService( intent );
+												}
+												
+											} );		
 
-											}
-										} );
-		
-		
-		m_offButton.setOnClickListener( new OnClickListener()
-										{
-											public void onClick( View v )
-											{
-												Log.i( TAG, "onClick" );
-
-												Context	ctx		= getApplicationContext();
-												Intent	intent	= new Intent( ctx, BellService.class );
-
-												stopService( intent );
-
-											}
-										} );
-		
-		
 	}
 	
-	private Button	m_onButton;
-	private Button	m_offButton;
+	private boolean isServiceRunning( Class class_ )
+	{
+		boolean										result			= false;
+		String										className		= class_.getName();
+		ActivityManager								manager			= (ActivityManager) getSystemService( Context.ACTIVITY_SERVICE );
+		List<ActivityManager.RunningServiceInfo>	runningServices	= manager.getRunningServices( Integer.MAX_VALUE );
+		
+		for( ActivityManager.RunningServiceInfo service : runningServices )
+		{
+			String	serviceName	= service.service.getClassName();
+			
+			if( className.equals( serviceName ) )
+			{
+				result	= true;
+				
+				break;
+				
+			}
+			
+		}
+    
+		return result;
+		
+	}	
+	
+	private synchronized void ProcessOnClick()
+	{
+		Context			ctx			= getApplicationContext();
+		Intent			intent		= new Intent( ctx, BellService.class );
+		
+		if( true == m_isRunning )
+		{
+			stopService( intent );
+			m_isRunning	= false;
+			m_serviceButton.setText( CAPTION_ON );
+			
+		}
+		else
+		{
+			ComponentName	starResult	= startService( intent );
+			
+			if( null != starResult )
+			{
+				m_isRunning	= true;
+				m_serviceButton.setText( CAPTION_OFF );
+				
+			}
+			
+		}
+
+	}
+	
+	private Button	m_serviceButton;
+	private boolean	m_isRunning;
 	
 }
