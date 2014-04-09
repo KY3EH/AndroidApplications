c44aa931608068acf18ed5d5de385ba39b2d7c4f
KY3EH <ky3eh.home@gmail.com>
2014-04-09 23:09:14 +0400

    GPS provider

--- TripTracker/src/com/alma/android/triptracker/MainActivity.java
+++ TripTracker/src/com/alma/android/triptracker/MainActivity.java
@@ -1,6 +1,7 @@
 package com.alma.android.triptracker;
 
 import android.app.Activity;
+import android.location.LocationManager;
 import android.os.Bundle;
 
 public class MainActivity extends Activity
@@ -9,7 +10,23 @@ public class MainActivity extends Activity
     @Override
     public void onCreate(Bundle savedInstanceState)
     {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.main);
-    }
+        super.onCreate( savedInstanceState );
+        setContentView( R.layout.main );
+
+		m_service	= (LocationManager)getSystemService( LOCATION_SERVICE );
+
+		boolean	enabled	= m_service.isProviderEnabled( LocationManager.GPS_PROVIDER );
+
+		// check if enabled and if not send user to the GSP settings
+		// Better solution would be to display a dialog and suggesting to 
+		// go to the settings
+		if( true == enabled )
+		{
+			
+		}
+		
+	}
+	
+	private LocationManager	m_service;
+	
 }
